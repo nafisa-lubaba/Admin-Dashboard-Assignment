5f5c003b4987a9eaa94c23351f4cc4fb4a2289fa
Nafisa <nafisalubaba71@gmail.com>
2025-02-07 17:36:24 +0600

    modify-sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,42 +1,24 @@
-import { Link } from "react-router-dom";
-import { FaUserAlt } from "react-icons/fa";
-import { FaBox } from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
 
-
-const Sidebar = () => {
+export default function Sidebar() {
   return (
-    <div className="h-screen w-64 bg-teal-600 text-white p-5">
-
-
-      <h2 className="text-xl font-bold mb-4 ">Admin Dashboard</h2>
+    <div className="w-64 bg-teal-600 min-h-screen p-4">
+      <h2 className="text-white text-2xl mb-6">Admin Dashboard</h2>
       <hr className="border-dashed border-white p-5" />
-      <div className="border-dashed border-white">
-
-      </div>
       <nav>
-        <ul>
-          <li className="mb-2">
-            <div className="flex gap-3 ">
-              <div><FaUserAlt />
-              </div>
-              <Link to="/" className="hover:text-gray-300">Users</Link>
-
-            </div>
-
-
-          </li>
-          <li className="mb-2">
-           <div className="flex gap-3">
-            <div>
-            <FaBox />
-            </div>
-           <Link to="/products" className="hover:text-gray-300">Products</Link>
-           </div>
-          </li>
-        </ul>
+        <NavLink 
+          to="/" 
+          className={({ isActive }) => `block p-2 text-teal-50 text-lg rounded mb-2 ${isActive ? 'bg-teal-500' : 'hover:bg-teal-500'}`}
+        >
+          Users
+        </NavLink>
+        <NavLink
+          to="/products"
+          className={({ isActive }) => `block p-2 text-teal-50 text-lg rounded mb-2 ${isActive ? 'bg-teal-500' : 'hover:bg-teal-500'}`}
+        >
+          Products
+        </NavLink>
       </nav>
     </div>
   );
-};
-
-export default Sidebar;
+}
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCs-PBSAYlnLueC7phihzWpKYtnC2gfrKs",
+  authDomain: "admin-dashboard-2c9e9.firebaseapp.com",
+  projectId: "admin-dashboard-2c9e9",
+  storageBucket: "admin-dashboard-2c9e9.firebasestorage.app",
+  messagingSenderId: "485958260994",
+  appId: "1:485958260994:web:8279953b9bf0183125ca1b"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
